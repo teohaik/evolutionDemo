a63921227806796d4a10117513a0a1b22808d353
Theodore Chaikalis <teohaik@gmail.com>
2015-02-25 21:42:44 +0200

    version 5

---
+++ src/package1/Class7.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class Class7 {
+	
+	Class1 ref1;
+	Class2 ref2;
+	package2.Class7 refp2c7;
+}
---
+++ src/package2/Class7.java
@@ -0,0 +1,6 @@
+package package2;
+
+public class Class7 {
+	Class3 ref3;
+	package1.Class7 refp1c7;
+}
